9c6eb2bfa0ba67addbc9ed69d7dbe23c0c77bcaf
SHERRY <sherry.feng@duke.edu>
2018-04-29 18:44:04 -0400

    fixed admin route error

--- router-app/src/router/index.js
+++ router-app/src/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import Home from '@/components/Home'
 import Login from '@/components/Login'
 import User from '@/components/User'
+import Admin from '@/components/Admin'
 
 Vue.use(Router)
 
